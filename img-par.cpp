f06b94b086a1f1b1c094d1887796f9e2c7fb1670
Enrique Lozano <61509169+enriqueloz88@users.noreply.github.com>
2020-12-06 00:22:58 +0100

    Code structure changed

--- img-par.cpp
+++ img-par.cpp
@@ -7,129 +7,24 @@
 using namespace std;
 using namespace std::chrono;
 using clk = chrono::high_resolution_clock;
+namespace fs = std::filesystem;
 
-unsigned char * gauss(unsigned char * data, int width, int height){
-    int w = 273; 
-    if (height==0 || width==0){
-        cout << "Error, heigth or width of the image is 0";
+void filtroGauss(string ficheroBmp, string outFile, string mode){
+    FILE* f;
+    cout << "filtrando: " << ficheroBmp.c_str() << endl;
+
+    if(fopen(ficheroBmp.c_str(), "rb") == nullptr){
+        cout << "File could not be opened" << endl;
     }
-    //int pad = width + (width%4);
-    int mascara[5][5] = {{1,4,7,4,1},{4,16,26,16,4},{7,26,41,26,7},{4,16,26,16,4},{1,4,7,4,1}};
-    #pragma omp parallel
-    {
-    #pragma omp for
-    for (int i=0; i<height; i++){
-        for (int j=0; j<width; j++){
-            
-            int valueBlue = 0, valueGreen = 0, valueRed=0; // Variables que contendran el nuevo color de cada posición
-            
-            for (int s=0; s<=4; s++){
-                for (int t=0; t<=4; t++){
-                    // Cogemos el dato de cada color en cada pixel. Si esta fuera de la imagen ese color sera 0
-                    int pixelDataBlue, pixelDataGreen, pixelDataRed;
-                    if ((i-2+s)>=height || (j-2+t)>=width || (i-2+s)<0 || (j-2+t)<0){
-                        pixelDataBlue = 0;
-                        pixelDataGreen = 0;
-                        pixelDataRed = 0;
-                    }else{
-                        pixelDataBlue = (int)data[3 * ((i-2+s) * width + (j-2+t))]; 
-                        pixelDataGreen = (int)data[3 * ((i-2+s) * width + (j-2+t)) + 1];
-                        pixelDataRed = (int)data[3 * ((i-2+s) * width + (j-2+t)) + 2];   
-                    }
-                    // Cogemos la posición correspondiente a la mascara, para posteriormente aplicar la formula
-                    int mascData = mascara[s][t];
-                    valueBlue = mascData*pixelDataBlue + valueBlue;    
-                    valueGreen = mascData*pixelDataGreen + valueGreen;   
-                    valueRed = mascData*pixelDataRed + valueRed;            
-                }
-            }
-            valueBlue = valueBlue / w;
-            valueGreen = valueGreen / w;
-            valueRed = valueRed / w;
-            // Asignamos los nuevos colores a la posición de la imagen en la que nos encontramos
-            data[3 * (i * width + j)] = (unsigned char) valueBlue;
-            data[3 * (i * width + j) + 1] = (unsigned char) valueGreen;
-            data[3 * (i * width + j) + 2] = (unsigned char) valueRed;
-        }    
-    }
-    
-    }//Pragma ends
-    
-    return data;
-}
+    f = fopen(ficheroBmp.c_str(),"rb");
 
-unsigned char * sobel(unsigned char * data, int width, int height){
-    int w = 8;
-    if (height==0 || width==0){
-        cout << "Error, heigth or width of the image is 0";
-    }
-    int m_x[3][3] = {{1,2,1},{0,0,0},{-1,-2,-1}};
-    int m_y[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}};
-    
-    #pragma omp parallel
-    {
-    #pragma omp for
-    for (int i=0; i<height; i++) {
-        for (int j=0; j<width; j++) {
-            
-            // Variables que contendran el nuevo color de cada posición:
-            int valueBlueX = 0, valueGreenX = 0, valueRedX =0;
-            int valueBlueY = 0, valueGreenY = 0, valueRedY =0;
-            
-            for (int s=0; s<=2 ; s++) {
-                for (int t=0; t<=2 ; t++) {
-                    // Cogemos el dato de cada color en cada pixel. Si esta fuera de la imagen ese color sera 0
-                    int pixelDataBlue, pixelDataGreen, pixelDataRed;
-                    if ((i-1+s)>=height || (j-1+t)>=width || (i-1+s)<0 || (j-1+t)<0){
-                        pixelDataBlue = 0;
-                        pixelDataGreen = 0;
-                        pixelDataRed = 0;
-                    }else{
-                        pixelDataBlue = (int)data[3 * ((i-1+s) * width + (j-1+t))]; 
-                        pixelDataGreen = (int)data[3 * ((i-1+s) * width + (j-1+t)) + 1];
-                        pixelDataRed = (int)data[3 * ((i-1+s) * width + (j-1+t)) + 2];  
-                    }
-                    // Cogemos la posición correspondiente a la mascara, para posteriormente aplicar la formula
-                    int mascData_x = m_x[s][t];
-                    int mascData_y = m_y[s][t];
-                    
-                    valueBlueX = mascData_x*pixelDataBlue + valueBlueX;
-                    valueGreenX = mascData_x*pixelDataGreen + valueGreenX;
-                    valueRedX = mascData_x*pixelDataRed + valueRedX;
+    unsigned char header[54];
+    int read;
+    read = fread(header, sizeof(unsigned char), 54, f);
 
-                    valueBlueY = mascData_y*pixelDataBlue + valueBlueY;
-                    valueGreenY = mascData_y*pixelDataGreen + valueGreenY;
-                    valueRedY = mascData_y*pixelDataRed + valueRedY;
-                }
-            }
-
-            int valueBlue = (abs(valueBlueX)+abs(valueBlueY)) / w;
-            int valueGreen = (abs(valueGreenX)+abs(valueGreenY)) / w;
-            int valueRed = (abs(valueRedX)+abs(valueRedY)) / w;
-            // Asignamos los nuevos colores a la posición de la imagen en la que nos encontramos
-            data[3 * (i * width + j)] = (unsigned char) valueBlue;
-            data[3 * (i * width + j) + 1] = (unsigned char) valueGreen;
-            data[3 * (i * width + j) + 2] = (unsigned char) valueRed;
-        }
-    }}
-    return data;
-}
-
-unsigned char* doOperation(string filename, string exitFile, string operation)
-{
-    auto t1_Load = clk::now();
-    FILE* f = fopen(filename.c_str(), "rb");
-    unsigned char info[54];
-
-    // read the 54-byte header
-    size_t result = fread(info, sizeof(unsigned char), 54, f);
-    if (result<=0){ 
-        cerr<<"Error in function fread"<<endl;
-    }
-
-    uint16_t nplano = *(int*)&info[26];        // uint16 -> 2 Bytes
-    uint16_t spunto = *(int*)&info[28];
-    uint32_t vcompresion = *(int*)&info[30];   // uint32 -> 4 Bytes
+    uint16_t nplano = *(int*)&header[26];        // uint16 -> 2 Bytes
+    uint16_t spunto = *(int*)&header[28];
+    uint32_t vcompresion = *(int*)&header[30];   // uint32 -> 4 Bytes
 
     if(nplano !=1){
         cerr << "Wrong format of BMP image. Number of color planes should be 1." << endl;
@@ -142,142 +37,146 @@ unsigned char* doOperation(string filename, string exitFile, string operation)
     }
 
     // Extract image height and width from header
-    int width = *(int*)&info[18];
-    int height = *(int*)&info[22];
-    int startInfo = *(int*)&info[10] - 54;
-    int row_padded = (width*3 + 3) & (~3);
+    int width = *(int*)&header[18];
+    int height = *(int*)&header[22];
+    int startInfo = *(int*)&header[10];
+    int imageSize = *(uint32_t*)&header[34];
 
-    cout << "Width->" << width*3 << endl;
-    cout << "Width con padding->" << row_padded << endl;
-    cout << "Se han añadido: " << width%4 << endl;
+    int cerosFila = (imageSize - width*height*3)/height;
 
-    // allocate 3 bytes per pixel
-    int size = 3 * row_padded * height;
-    unsigned char* dataUntilStart = new unsigned char[startInfo]; 
-    unsigned char* dataRow = new unsigned char[row_padded]; //Contiene los colores de una sola fila
-    unsigned char* data = new unsigned char[size];          //Array de Colores-> Suma de todos los dataRow
+    unsigned char *demas = new unsigned char[startInfo-54];
+    read = fread(demas, sizeof(unsigned char), startInfo-54, f);
+    delete(demas);
 
-    // Leemos la cadena situada continuación del header
-    result = fread(dataUntilStart, sizeof(unsigned char), startInfo, f); // Bytes extra antes de llegar a la info de los colores
-    int contador = 0;
-    for (int i = 0; i < height; i++){
-        result = fread(dataRow, sizeof(unsigned char), row_padded, f);   
-        if (result<=0){ 
-            cerr<<"Error in function fread"<<endl;
-        }
-        for (int j=0; j< row_padded; j++){
-            data[contador] = dataRow[j];   // Data contiene los colores
-            contador++;
-        }
-
+    unsigned char *img = new unsigned char[imageSize];
+    read = fread(img, sizeof(unsigned char), imageSize, f);
+    if (read != imageSize){
+        cout << "Read error" << endl;
     }
-/*
-    for (int i = 0; i < 200; i++)
-    {
-        cout << "i=" << i << ": "<<(int)data[i] << ",  ";
-    }
-    cout << "" << endl;
-*/
-    // Pixel(i,j) = data[3*(i*width+j)]
-    /* //Para imprimir los colores de cada pixel:
-    for (int i=0; i<height; i++){
-        for (int j=0; j<width; j++){
-            cout <<"i->"<< i<<"j--->"<< j<<"\t Blue: "<< (int)data[3 * (i * width + j)] << " Green: " << (int)data[3 * (i * width + j)+1]<< " Red: " << (int)data[3 * (i * width + j) +2]<< endl;
-        }
-    }
-    */
 
     fclose(f);
 
-    auto t2_Load = clk::now();
-    auto diff_Load = duration_cast<microseconds>(t2_Load - t1_Load);
-    double secsLoad = (double) (diff_Load.count());
-    double totalTime = secsLoad;
+    //NUEVA CABECERA
+    int fileSize = 54 + imageSize;
+    header[2] = fileSize%256;
+    header[3] = (fileSize/256)%256;
+    header[4] = (fileSize/65536)%256;
+    header[5] = fileSize/16777216;
 
-    //-----------LOAD ENDED--------------
-    //-----------------------------------
-    //-------OPERATIONS STARTING---------
+    header[6] = (uint32_t) 0;
 
-    double secsGauss, secsSobel;
-    if(strcmp(operation.c_str(), "gauss")==0){
-        auto t1_Gauss = clk::now();
-        
-        data = gauss(data, row_padded, height);
-        
-        auto t2_Gauss = clk::now();
-        auto diff_Gauss = duration_cast<microseconds>(t2_Gauss - t1_Gauss);
-        secsGauss = (double) (diff_Gauss.count());
-        totalTime = totalTime + secsGauss;
-    }
-    if(strcmp(operation.c_str(), "sobel")==0){
-        auto t1_Gauss = clk::now();
-        
-        data = gauss(data, row_padded, height);
-        
-        auto t2_Gauss = clk::now();
-        auto diff_Gauss = duration_cast<microseconds>(t2_Gauss - t1_Gauss);
-        secsGauss = (double) (diff_Gauss.count());
-        
-        auto t1_Sobel = clk::now();
+    //Inicio de datos de la imagen
+    header [10] = 54;
 
-        data = sobel(data, row_padded, height);
-        
-        auto t2_Sobel = clk::now();
-        auto diff_Sobel = duration_cast<microseconds>(t2_Sobel - t1_Sobel);
-        secsSobel = (double) (diff_Sobel.count());
-        totalTime = totalTime + secsGauss + secsSobel;
-    }
+    header[14] = 40;
+    header[15] = header[16] = header[17] = 0;
+    header[11] = header[12] = header[13] = 0;
 
-    auto t1_Store=clk::now();
-    
-    FILE* newf = fopen(exitFile.c_str(), "w");
-    
-    result = fwrite(info, sizeof(unsigned char), 54, newf);  //Write header info
-    result = fwrite(dataUntilStart, sizeof(unsigned char), startInfo, f);
-    result = fwrite(data, sizeof(unsigned char), size, newf);//Write pixels
-    if (result<=0){ 
-        cerr<<"Error in function fwrite"<<endl;
+    //Valores 2835
+
+    header[38] =  19;
+    header[39] =  11;
+    header[42] = 19;
+    header[43] = 11;
+
+    header[40] = 0;
+    header[41] = 0;
+    header[40] = 0;
+    header[41] = 0;
+    header[44] = 0;
+    header[45] = 0;
+
+    unsigned char **byte1 = new unsigned char *[height+4];
+    unsigned char **byte2 = new unsigned char *[height+4];
+    unsigned char **byte3 = new unsigned char *[height+4];
+    for(int i=0; i<(height+4);i++){
+        byte1[i] = new unsigned char[width+4];
+        byte2[i] = new unsigned char[width+4];
+        byte3[i] = new unsigned char[width+4];
     }
 
-    auto t2_Store = clk::now();
-    auto diff_Store = duration_cast<microseconds>(t2_Store - t1_Store);
-    double secsStore = (double) (diff_Store.count());
-    totalTime = totalTime + secsStore;
-
-    //------------Printing the time----------
-    cout << " (time: " << totalTime << ")" << endl;
-    cout << " Load time: " << secsLoad << endl;
-    if(strcmp(operation.c_str(), "gauss")==0){
-        cout << " Gauss: " << secsGauss << endl;
-    }else if(strcmp(operation.c_str(), "sobel")==0){
-        cout << " Gauss: " << secsGauss << endl;
-        cout << " Sobel: " << secsSobel << endl;
-    }
-    cout << " Store time: " << secsStore << endl;
-
-    return data;
-}
-
-// Función que crea los nombres de los nuevos ficheros a crear.
-string exitFileToString(string entryfile){
-    string delim = "/";
-
-    auto start = 0U;
-    auto end = entryfile.find(delim);
-    while (end != string::npos)
-    {
-        //cout << s.substr(start, end - start) << endl;
-        start = end + delim.length();
-        end = entryfile.find(delim, start);
+    int indice = 0;
+    for(int i= 2; i < height+2;i++){
+        if (i>2){
+            indice = indice + cerosFila;
+        }
+        for(int j=2; j<width+2; j++){
+            *(*(byte1+i)+j) = img[indice];
+            *(*(byte2+i)+j) = img[indice+1];
+            *(*(byte3+i)+j) = img[indice+2];
+            indice = indice+3;
+        }
     }
 
-    return entryfile.substr(start, end);
+    unsigned char **g1 = new unsigned char *[height+4];
+    unsigned char **g2 = new unsigned char *[height+4];
+    unsigned char **g3 = new unsigned char *[height+4];
+    for(int i=0; i<(height+4);i++){
+        g1[i] = new unsigned char[width+4];
+        g2[i] = new unsigned char[width+4];
+        g3[i] = new unsigned char[width+4];
+    }
+
+    if(mode.compare("gauss")==0 || mode.compare("sobel")==0){
+        
+        //GAUSS
+        int masc[5][5] = {{1,4,7,4,1},{4,16,26,16,4},{7,26,41,26,7},{4,16,26,16,4},{1,4,7,4,1}};
+        int w = 273;
+
+        float a = 0, b=0, c=0;
+
+        for(int i=1; i<height+1; i++){
+            for(int j=1; j<width+1; j++){
+                for(int s=0; s<5; s++){
+                    for(int t=0; t<5; t++){
+                        a+=(masc[s][t]) * (*(*(byte1 + i + s - 1) + (j+t-1)));
+                        b+=(masc[s][t]) * (*(*(byte2 + i + s - 1) + (j+t-1)));
+                        c+=(masc[s][t]) * (*(*(byte3 + i + s - 1) + (j+t-1)));
+                    }
+                }
+                a = a / w;
+                b = b / w;
+                c = c / w;
+                *(*(g1+i)+j) = (unsigned char) (a);
+                *(*(g2+i)+j) = (unsigned char) (b);
+                *(*(g3+i)+j) = (unsigned char) (c);
+                a = b = c = 0;
+            }
+        }
+
+        if (mode.compare("sobel")==0){
+            //SOBEL
+            cout << "doing sobel" << endl;
+        }
+    }
+
+    indice = 0;
+    for(int i= 0; i < height;i++){
+        if (i>0){
+            indice = indice + cerosFila;
+        }
+        for(int j=0; j<width; j++){
+            img[indice] = *(*(g1+i+1)+(j+1));
+            img[indice + 1] = *(*(g2+i+1)+(j+1));
+            img[indice + 2] = *(*(g3+i+1)+(j+1));
+
+            indice = indice+3;
+        }
+    }
+
+    FILE* output = fopen(outFile.c_str(), "wb");
+    fseek(output, 0, SEEK_SET);
+
+    fwrite(header,sizeof(unsigned char), 54, output);
+    fwrite(img, sizeof(unsigned char), imageSize, output);
+
+    delete[] img;
+    fclose(output);
+
 }
 
 
 int main(int nargs, char *args[]){
-    omp_set_num_threads(4);
-
     if(nargs != 4){ //Son necesarios 4 argumentos. P.ej.---> ./image-seq gauss indir outdir
         cerr<<"Wrong format:"<<endl;
         cerr<<"image-seq operation in_path out_path;" <<endl;
@@ -317,59 +216,19 @@ int main(int nargs, char *args[]){
     cout << "Input path: " << args[2] << endl;
     cout << "Output path: " << args[3] << endl;
 
-    const auto root = filesystem::current_path(); // Directorio actual
-    const auto target = root / args[3];   // Directorio salida = "Actual/dir_out"
+    string dir_in_str = args[2];
+    string dir_out_str = args[3];
+    string filePathOrigin = dir_in_str + "/";
+    for (const auto &entry : fs::directory_iterator(filePathOrigin)){
+        filePathOrigin = entry.path().string();
+        cout << "File: " << filePathOrigin << endl;
+        int pos = filePathOrigin.find("/");
+        string filePathDestiny = dir_out_str + "/" + filePathOrigin.substr(pos+1);
+        cout << "File Destiny: " << filePathDestiny << endl;
 
-    string path = args[2]; //Directorio de entrada
-
-    //REALIZAMOS UNA DE LAS TRES OPERACIONES:
+        filtroGauss(filePathOrigin, filePathDestiny, "gauss");
+    }
     
-    if(strcmp(args[1], "copy")==0) { //La operación es copy
-        for (const auto &entry : filesystem::directory_iterator(path)) {
-
-            cout << "File: " << entry.path();
-
-            string entryfile = entry.path().string();
-
-            string filename = exitFileToString(entryfile);  // Filename of new file. It will have the same name as entryfile
-            string exitFolderName(args[3]);                 // Folder of new file
-            const auto exitFile = exitFolderName + "/" + filename;
-
-            doOperation(entryfile, exitFile, "copy");  // Path to string
-        }
-    }
-
-    else if(strcmp(args[1], "gauss")==0){ //La operación es gauss
-        for (const auto &entry : filesystem::directory_iterator(path)) {
-            cout << "File: " << entry.path();
-
-            string entryfile = entry.path().string();
-
-            string filename = exitFileToString(entryfile); // Filename of new file. It will have the same name as entryfile
-            string exitFolderName(args[3]);                 // Folder of new file
-            const auto exitFile = exitFolderName + "/" + filename;
-
-            doOperation(entryfile, exitFile, "gauss");  // Path to string
-        }
-
-    }
-
-    else if(strcmp(args[1], "sobel")==0){ //La operación es gauss
-        for (const auto &entry : filesystem::directory_iterator(path)) {
-            cout << "File: " << entry.path();
-
-            string entryfile = entry.path().string();
-
-            string filename = exitFileToString(entryfile);; // Filename of new file. It will have the same name as entryfile
-            string exitFolderName(args[3]);                 // Folder of new file
-            const auto exitFile = exitFolderName + "/" + filename;
-
-            doOperation(entryfile, exitFile, "sobel");  // Path to string
-        }
-
-    }
-
 
     return 0;
-
 }
